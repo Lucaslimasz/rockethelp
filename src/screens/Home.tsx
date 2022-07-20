39687ed580362aae2ba20b1191bbe7f1542ec293
Lucas Lima <id.lucas@outlook.com.br>
2022-07-19 23:59:09 -0300

    starting firebase configuration

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -66,7 +66,7 @@ export default function Home() {
           alignItems="center"
         >
           <Heading color="gray.100">Solicitações</Heading>
-          <Text color="gray.200">3</Text>
+          <Text color="gray.200">{orders.length}</Text>
         </HStack>
         <HStack space={3} mb={8}>
           <Filter
